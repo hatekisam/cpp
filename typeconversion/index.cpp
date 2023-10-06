105a550e86a84ccff2d84712acdb075fa67d3fee
mugisha <mugisha@(none)>
2023-10-06 15:54:07 +0200

    studies the implicit and explicit type conversion

---
+++ typeconversion/index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+
+    // type conversion ca be done implicit
+
+    double x = (int)3.14;
+    std::cout << x << std::endl;
+
+    // type conversion ca be done implicit
+
+    char ab =  100;
+
+    //it will print d because d is 100 in ascii
+    std::cout << ab << std::endl;
+
+
+    return 0;
+}
