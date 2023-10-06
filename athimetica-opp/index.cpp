16bfb218a437547e0aaca7807238cc1ee0c1488e
mugisha <mugisha@(none)>
2023-10-06 15:41:44 +0200

    studies the arthimetic operation s

---
+++ athimetica-opp/index.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+
+    // Addtion
+    int a = 6 + 7;
+    a++;
+    a += 10;
+    std::cout << a << std::endl;
+    // Subtraction
+    int b = 16 - 7;
+    b--;
+    a -= 2;
+    std::cout << b << std::endl;
+    // Multiplication
+    int c = 2 * 3;
+    c *= 2;
+    std::cout << c << std::endl;
+    // Division
+    int d = 16 / 2;
+    d /= 2;
+    std::cout << d << std::endl;
+    // Modulus
+    int e = 16 % 2;
+    e %= 2;
+    std::cout << e << std::endl;
+
+    return 0;
+}
