fdef82c3b396c9314f5a5384a9dde48b729e379b
mugisha <mugisha@(none)>
2023-10-06 12:18:00 +0200

    finished variables and datatypes

--- variables-datatypes/index.cpp
+++ variables-datatypes/index.cpp
@@ -2,7 +2,29 @@
 
 int main()
 {
+    // integers
     int x = 5;
+
+    // if  you create an integer and put decimals the deciaml part will be removed
+
+    int y = 5.6;
+
+    // numbers with decimals
+    double price = 23.43;
+
+    // characters
+
+    char grade = 'A';
+
+    // booleans
+
+    bool active = true;
+
+    // string
+
+    std::string name = "Mugisha Yves";
+
+    std::cout << "Hello" << name << std::endl;
 
     std::cout << x;
     return 0;
